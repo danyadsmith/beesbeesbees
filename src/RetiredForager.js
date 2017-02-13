08109a3228f68125117ef27ce8df01e4ec33d831
Danya D. Smith <danyadsmith@gmail.com>
2017-02-13 18:18:31 -0500

    RETIRED: Implements the Retired Forager Bee class

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,22 @@
-var RetiredForagerBee = function() {
+/* globals ForagerBee */
+
+var RetiredForagerBee = function () {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+
+RetiredForagerBee.prototype.forage = function (treasure) {
+  this.thinking = 'I am to old to forage for ' + treasure;
+  return 'I am too old, let me play cards instead';
+};
+
+RetiredForagerBee.prototype.gamble = function (retirementSavings) {
+  this.treasureChest.push(retirementSavings * 2);
+};
